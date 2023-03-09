c7ffdc5ceacc162b8c582c8efa4f625c520f75d1
Brian Joo <brianhjoo@gmail.com>
2023-03-08 16:52:23 -0800

    finished part one

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story, excited_story
+from stories import silly_story as story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
-@app.get('/silly-story')
+@app.get('/')
 def create_silly_story():
+    """ renders a form that accepts words for the story's prompt """
 
-    return render_template("questions.html",
-                           prompts = silly_story.prompts)
-
-@app.get('/excited-story')
-def create_excited_story():
-
-    return render_template("questions.html",
-                           prompts = excited_story.prompts)
+    return render_template(
+        "questions.html",
+        prompts=story.prompts)
 
 @app.get('/results')
 def show_results():
+    """ renders the content with the user's provided madlib inputs """
 
-    print(request)
-
-    return render_template("results.html")
+    return render_template(
+        "results.html",
+        content=story.generate(request.args)
+    )
