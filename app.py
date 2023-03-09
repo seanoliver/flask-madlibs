71541145cff0bffeec0ac62d4e59db3def0c3ea0
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-03-08 16:09:06 -0800

    implemented the questions form; started working on the results page

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import silly_story, excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get('/silly-story')
+def create_silly_story():
+
+    return render_template("questions.html",
+                           prompts = silly_story.prompts)
+
+@app.get('/excited-story')
+def create_excited_story():
+
+    return render_template("questions.html",
+                           prompts = excited_story.prompts)
+
+@app.get('/results')
+def show_results():
+
+    print(request)
+
+    return render_template("results.html")
