685a6d36ef509f8d168eb0fc2658c6bb1154acc4
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-03-23 22:30:07 -0700

    refactored to only allow AI generated stories

--- app.py
+++ app.py
@@ -1,47 +1,36 @@
 import sys
 print(sys.executable)
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 
-# from stories import silly_story as story
-from stories import silly_story, excited_story, ai_story
+from stories import ai_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
-stories = {
-        "Silly Story" : silly_story,
-        "Excited Story" : excited_story,
-        "AI-Generated Story" : ai_story,
-    }
-
 @app.get('/')
 def create_silly_story():
     """ renders a form that accepts words for the story's prompt """
 
-    return render_template(
-        "story_selector.html",
-        templates=stories.keys()
-    )
+    return redirect('/questions')
 
 
 @app.get('/questions')
 def story_questions():
     """ renders the list of questions for the user to populate the story"""
 
-    story = stories[request.args['template']]
+    story = ai_story
     return render_template(
         "questions.html",
-        template=request.args['template'],
-        prompts=story.prompts)
+        prompts=story.uniques)
 
-@app.get('/results/<template>')
-def show_results(template):
+@app.get('/result')
+def show_results():
     """ renders the content with the user's provided madlib inputs """
-    story = stories[template]
+    story = ai_story
 
     return render_template(
         "results.html",
